20fcc4599ac11f2251401e383eb99a75419caa0d
Kevin Chu <kvnchu2@gmail.com>
2020-11-24 00:59:18 +0000

    added in some template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🙂 Assertion Passed: ' + actual + '=== ' + expected);
+    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('😲 Assertion Failed: ' + actual + '!== ' + expected);
+    console.log(`😲 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
