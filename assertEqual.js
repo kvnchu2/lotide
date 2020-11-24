7cd2ca8b6fc6717a61d07b5b663c412298d0103e
Kevin Chu <kvnchu2@gmail.com>
2020-11-24 00:35:58 +0000

    added function for comparing two strings

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🙂 Assertion Passed: ' + actual + '=== ' + expected);
+  } else {
+    console.log('😲 Assertion Failed: ' + actual + '!== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
