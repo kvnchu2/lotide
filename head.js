336bd790b58ebe7cda2521e064c383e3fd657369
Kevin Chu <kvnchu2@gmail.com>
2020-11-24 01:19:45 +0000

    added head function and test cases

---
+++ head.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `🙂 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return`😲 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const head = function (array) {
+  let firstElement = array[1];
+  return firstElement;
+}
+
+// TEST CODE
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
