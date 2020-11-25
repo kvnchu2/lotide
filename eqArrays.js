ad95f654d3f070b06053988c18f51746c397030a
Kevin Chu <kvnchu2@gmail.com>
2020-11-25 01:28:15 +0000

    added in eqArray function

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😲 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne,arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false
+  } 
+  for (let element = 0; element < arrayOne.length; element++) {
+      if (arrayOne[element] !== arrayTwo[element]) {
+        return false;
+      } 
+  } 
+  return true;
+}
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
