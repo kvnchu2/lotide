f5b0db0b5c380e2e1c4ec4880f82d0055f8cd80f
Kevin Chu <kvnchu2@gmail.com>
2020-11-25 17:08:48 +0000

    added flatten assignment

---
+++ flatten.js
@@ -0,0 +1,18 @@
+const flatten = function(array) {
+  let flattenedArray = [];
+  for (let element = 0; element < array.length; element++) {
+    if (Array.isArray(array[element])) {
+      for (let element2 = 0; element2 < array[element].length; element2++) {
+        flattenedArray.push(array[element][element2]);
+      }
+    } else {
+      flattenedArray.push(array[element]);
+    }
+
+  }
+
+  return flattenedArray;
+}
+
+
+console.log(flatten(([1, 2, [3, 4], 5, [6]])));
