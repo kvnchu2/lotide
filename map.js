8864b8053c16004f7054962c07602e6851a5743d
Kevin Chu <kvnchu2@gmail.com>
2020-11-26 21:16:24 +0000

    added map assignment

---
+++ map.js
@@ -0,0 +1,42 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+
+const results1 = map(words, word => word[0]);
+
+let assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😲 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+}
+
+
+
+const eqArrays = function(arrayOne,arrayTwo) {
+  if(arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let element = 0; element < arrayOne.length; element++) {
+    if (arrayOne[element] !== arrayTwo[element]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+console.log(assertArraysEqual(results1, ['g','c','t','m','t']))
