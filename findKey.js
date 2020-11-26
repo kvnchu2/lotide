2e296e83b3c663af079356b7cc9e50eec43b8e24
Kevin Chu <kvnchu2@gmail.com>
2020-11-26 22:47:39 +0000

    added findkey assignment

---
+++ findKey.js
@@ -0,0 +1,49 @@
+let assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😲 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+}
+
+
+
+const eqArrays = function(arrayOne,arrayTwo) {
+  if(arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let element = 0; element < arrayOne.length; element++) {
+    if (arrayOne[element] !== arrayTwo[element]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+
+
+
+const findKey = function(obj,func) {
+  for (let str in obj) {
+    if(func(obj[str])){
+      return str;
+    }
+  }
+}
+
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+
+console.log(assertArraysEqual(result1,'noma'));
