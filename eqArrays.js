5a6a92217b830b8c5ba068860451eda70daf70c9
Kevin Chu <kvnchu2@gmail.com>
2020-12-28 02:14:04 +0000

    edit eqArrays to accept arrays with nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,35 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = require('./assertEqual.js')
+const assertEqual = require('./assertEqual.js');
 
 const eqArrays = function(arrayOne,arrayTwo) {
-    if(arrayOne.length !== arrayTwo.length) {
+  //stores all the true and false values to be searched at the end 
+  let binaryArr = [];
+  //checks to see if the length of the two arrays are equal
+  if(arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  //checks to see if corresponding index element of arrayOne and arrayTwo are equal
+  for (let element = 0; element < arrayOne.length; element++) {
+    if (Array.isArray(arrayOne[element]) && Array.isArray(arrayTwo[element])) {
+      binaryArr.push(eqArrays(arrayOne[element], arrayTwo[element]))
+    } else if (arrayOne[element] !== arrayTwo[element]) {
+      return false;
+    } else if (typeof arrayOne[element] !== typeof arrayTwo[element]) {
       return false;
     }
-    for (let element = 0; element < arrayOne.length; element++) {
-      if (arrayOne[element] !== arrayTwo[element]) {
-        return false;
-      }
-    }
+  }
+
+  if (binaryArr.indexOf(false) >= 0) {
+    return false;
+  } else {
     return true;
+  }
 };
 
+
+console.log(assertEqual(eqArrays([[2, 3], [4, [5]]], [[2, 3], [4, [5, 6]]]), false)) // => false
+
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)) // => false
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)) // => false
 
 module.exports = eqArrays;
